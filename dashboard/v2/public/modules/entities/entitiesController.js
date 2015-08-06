0da661371973e97a6f2f147f84d718ff2936097f
DarshanKumar <darshankumar38@gmail.com>
2015-08-07 00:55:27 +0530

    Updated create entities
    Updated create entities

--- dashboard/v2/public/modules/entities/entitiesController.js
+++ dashboard/v2/public/modules/entities/entitiesController.js
@@ -18,60 +18,108 @@
 
 'use strict';
 
-angular.module('dgc.entities').controller('EntityController', ['$scope', '$resource', '$state', '$stateParams','lodash', 'EntityResource','NotificationService',
-    function($scope, $resource, $state, $stateParams, _, EntityResource,NotificationService) {
+angular.module('dgc.entities').controller('EntityController', ['$scope', '$resource', '$state', '$stateParams', 'lodash', 'EntityResource', 'NotificationService',
+    function($scope, $resource, $state, $stateParams, _, EntityResource, NotificationService) {
 
+        $scope.viewtype = "LoadProcess";
+        $scope.getviewtype = function() {
+            switch (Number($scope.category)) {
+
+                case 3:
+                    $scope.viewtype = "LoadProcess";
+                    break;
+                case 4:
+                    $scope.viewtype = "DB";
+                    break;
+
+            }
+
+        };
         $scope.appForm = {
             submit: function() {
-                var formData = null;
-                if ($scope.entitiyName) {
+                switch (Number($scope.category)) {
+                    case 1:
+                        NotificationService.reset();
+                        NotificationService.error('API not Available', false);
+                        break;
+                    case 2:
+                        NotificationService.reset();
+                        NotificationService.error('API not Available', false);
+                        break;
+                    case 3:
+                        NotificationService.reset();
+                        console.log($scope.entitiyName + "entity name");
+                        var formData = {
+                            "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                            "id": {
+                                "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                "id": "-1436980375639783000",
+                                "version": 0,
+                                "typeName": $scope.typeName
+                            },
+                            "typeName": $scope.typeName,
+                            "values": {
+                                "queryId": "id",
+                                "name": $scope.entitiyName,
+                                "startTime": 1436980375639,
+                                "queryPlan": $scope.queryPlan,
+                                "description": $scope.description,
+                                "outputs": [],
+                                "endTime": 1436980385639,
+                                "inputs": [],
+                                "queryText": $scope.queryText,
+                                "user": $scope.user,
+                                "queryGraph": "graph"
+                            },
+                            "traitNames": [
+                                "ETL"
+                            ],
+                            "traits": {
+                                "ETL": {
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Struct",
+                                    "typeName": "ETL",
+                                    "values": {
 
-                            NotificationService.reset();
-                            formData = {
-                                "jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
-                                "id":{
-                                    "jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
-                                    "id":"-1436980375639783000",
-                                    "version":0,
-                                    "typeName":$scope.typeName
-                                },
-                                "typeName":$scope.typeName,
-                                "values":{
-                                    "queryId":"id",
-                                    "name":$scope.entitiyName,
-                                    "startTime":1436980375639,
-                                    "queryPlan":$scope.queryPlan,
-                                    "description":$scope.description,
-                                    "outputs":[
-                                    ],
-                                    "endTime":1436980385639,
-                                    "inputs":[
-                                    ],
-                                    "queryText":$scope.queryText,
-                                    "user":$scope.user,
-                                    "queryGraph":"graph"
-                                },
-                                "traitNames":[
-                                    "ETL"
-                                ],
-                                "traits":{
-                                    "ETL":{
-                                        "jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct",
-                                        "typeName":"ETL",
-                                        "values":{
-
-                                        }
                                     }
                                 }
-                            };
+                            }
+                        };
+                        break;
+                    case 4:
+                        NotificationService.reset();
 
-                    }
-                 else {
-                    NotificationService.error('Enter Type Name', false);
+                        formData = {
+                            "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                            "id": {
+                                "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                "id": "-1438867091050961000",
+                                "version": 0,
+                                "typeName": $scope.typeName
+                            },
+                            "typeName": $scope.typeName,
+                            "values": {
+                                "name": $scope.name,
+                                "createTime": 1438867091050,
+                                "description": $scope.description,
+                                "locationUri": $scope.locationUri,
+                                "owner": $scope.owner
+                            },
+                            "traitNames": [
+
+                            ],
+                            "traits": {
+
+                            }
+                        };
+                        break;
+                    default:
+                        formData = null;
+                        NotificationService.reset();
+                        NotificationService.error('Enter Type Name', false);
                 }
                 if (typeof formData === "object" && !Array.isArray(formData) && formData !== null) {
-                    EntityResource.add(JSON.stringify(formData), function () {
-                        NotificationService.info('New entity has been created', false);
+                    EntityResource.add(JSON.stringify(formData), function() {
+                        $state.go("search");
                     });
                 }
             }
