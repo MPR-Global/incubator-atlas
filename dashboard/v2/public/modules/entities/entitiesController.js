e173e9ad24ca7b9fa65af161f511afffef222548
DarshanKumar <darshankumar38@gmail.com>
2015-08-09 23:58:16 +0530

    Update Create entities for Type Table
    Update Create entities for Type Table

--- dashboard/v2/public/modules/entities/entitiesController.js
+++ dashboard/v2/public/modules/entities/entitiesController.js
@@ -25,6 +25,9 @@ angular.module('dgc.entities').controller('EntityController', ['$scope', '$resou
         $scope.getviewtype = function() {
             switch (Number($scope.category)) {
 
+                case 2:
+                    $scope.viewtype = "Table";
+                    break;
                 case 3:
                     $scope.viewtype = "LoadProcess";
                     break;
@@ -44,12 +47,164 @@ angular.module('dgc.entities').controller('EntityController', ['$scope', '$resou
                         break;
                     case 2:
                         NotificationService.reset();
-                        NotificationService.error('API not Available', false);
+
+                        var formData = {
+                            "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                            "id": {
+                                "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                "id": "-1436884922170366000",
+                                "version": 0,
+                                "typeName": "Table"
+                            },
+                            "typeName": "Table",
+                            "values": {
+                                "tableType": "Managed",
+                                "name": $scope.TableName,
+                                "createTime": 1436884922170,
+                                "description": $scope.Tabledescription,
+                                "db": {
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                    "id": $stateParams.id,
+                                    "version": 0,
+                                    "typeName": "DB"
+                                },
+                                "retention": 1436884922170,
+                                "columns": [{
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                                    "id": {
+                                        "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                        "id": "-1436884922170004000",
+                                        "version": 0,
+                                        "typeName": "Column"
+                                    },
+                                    "typeName": "Column",
+                                    "values": {
+                                        "comment": "time_id",
+                                        "name": "time_id",
+                                        "dataType": "int"
+                                    },
+                                    "traitNames": [
+
+                                    ],
+                                    "traits": {
+
+                                    }
+                                }, {
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                                    "id": {
+                                        "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                        "id": "-1436884922170024000",
+                                        "version": 0,
+                                        "typeName": "Column"
+                                    },
+                                    "typeName": "Column",
+                                    "values": {
+                                        "comment": "product id",
+                                        "name": "product_id",
+                                        "dataType": "int"
+                                    },
+                                    "traitNames": [
+
+                                    ],
+                                    "traits": {
+
+                                    }
+                                }, {
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                                    "id": {
+                                        "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                        "id": "-1436884922170040000",
+                                        "version": 0,
+                                        "typeName": "Column"
+                                    },
+                                    "typeName": "Column",
+                                    "values": {
+                                        "comment": "customer id",
+                                        "name": "customer_id",
+                                        "dataType": "int"
+                                    },
+                                    "traitNames": [
+                                        "PII"
+                                    ],
+                                    "traits": {
+                                        "PII": {
+                                            "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Struct",
+                                            "typeName": "PII",
+                                            "values": {
+
+                                            }
+                                        }
+                                    }
+                                }, {
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                                    "id": {
+                                        "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                        "id": "-1436884922170294000",
+                                        "version": 0,
+                                        "typeName": "Column"
+                                    },
+                                    "typeName": "Column",
+                                    "values": {
+                                        "comment": "product id",
+                                        "name": "sales",
+                                        "dataType": "double"
+                                    },
+                                    "traitNames": [
+                                        "Metric"
+                                    ],
+                                    "traits": {
+                                        "Metric": {
+                                            "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Struct",
+                                            "typeName": "Metric",
+                                            "values": {
+
+                                            }
+                                        }
+                                    }
+                                }],
+                                "lastAccessTime": 1436884922170,
+                                "owner": $scope.Tableowner,
+                                "sd": {
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
+                                    "id": {
+                                        "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
+                                        "id": "-1436884922169953000",
+                                        "version": 0,
+                                        "typeName": "StorageDesc"
+                                    },
+                                    "typeName": "StorageDesc",
+                                    "values": {
+                                        "outputFormat": "TextOutputFormat",
+                                        "location": "hdfs://host:8000/apps/warehouse/sales",
+                                        "compressed": true,
+                                        "inputFormat": "TextInputFormat"
+                                    },
+                                    "traitNames": [
+
+                                    ],
+                                    "traits": {
+
+                                    }
+                                }
+                            },
+                            "traitNames": [
+                                "Fact"
+                            ],
+                            "traits": {
+                                "Fact": {
+                                    "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Struct",
+                                    "typeName": "Fact",
+                                    "values": {
+
+                                    }
+                                }
+                            }
+                        };
                         break;
                     case 3:
                         NotificationService.reset();
                         console.log($scope.entitiyName + "entity name");
-                        var formData = {
+                        formData = {
                             "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Reference",
                             "id": {
                                 "jsonClass": "org.apache.atlas.typesystem.json.InstanceSerialization$_Id",
