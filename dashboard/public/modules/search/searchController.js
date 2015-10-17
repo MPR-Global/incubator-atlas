e75ec9867a2c9b32ea666528fbe0ca92fd46bd47
shiv <shivchoudhary4109@gmail.com>
2015-10-18 01:20:36 +0530

    HDPDGI-170 -  HDPDGI-179 - Redesign Search Result in tabular form - UPDATED

--- dashboard/public/modules/search/searchController.js
+++ dashboard/public/modules/search/searchController.js
@@ -115,10 +115,6 @@ angular.module('dgc.search').controller('SearchController', ['$scope', '$locatio
             $scope.keyLength = count;
             return res;
         };    
-        angular.element(document).ready(function () {
-            $('[data-toggle="tooltip"]').tooltip(); 
-        });
-
         $scope.searchQuery = $location.search();
         $scope.query = ($location.search()).query;
         if ($scope.query) {
