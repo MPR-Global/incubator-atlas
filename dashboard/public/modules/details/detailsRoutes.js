9bf1f58b25c2b4ee2a2ba97c522860ad3986b45c
Darshan <darshankumar38@gmail.com>
2016-02-21 08:37:14 +0530

    schema add tag fixed

--- dashboard/public/modules/details/detailsRoutes.js
+++ dashboard/public/modules/details/detailsRoutes.js
@@ -32,14 +32,14 @@ angular.module('dgc.details').config(['$stateProvider',
                 tId: null,
                 frm : 'details'
             },
-            onEnter: ['$stateParams', '$state', '$modal', 'NavigationResource', function($stateParams, $state, $modal, NavigationResource) {
+            onEnter: ['$stateParams', '$state', '$modal', 'navigationResource', function($stateParams, $state, $modal, navigationResource) {
                 $modal.open({
                     templateUrl: '/modules/tags/instance/views/createTag.html',
-                    controller: 'CreateTagController',
+                    controller: 'createTagController',
                     windowClass: 'create-tag-entity',
                     resolve: {
                         typesList: function() {
-                            return NavigationResource.get().$promise;
+                            return navigationResource.get().$promise;
                         }
                     }
                 }).result.finally(function() {
